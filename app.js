ae5b0b9a60c5890cab8d69c18eb7f33ff61512f6
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-06-05 15:37:23 +0200

    Secret to .env, sessions and cookies implemented

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.set("view engine", "ejs");
 
 app.use(
   session({
-    secret: "Our little secret.",
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: false,
   })
@@ -85,8 +85,9 @@ app
   .post(async (req, res) => {
     try {
       await User.register({ username: req.body.username }, req.body.password);
-      await passport.authenticate("local");
-      res.redirect("/secrets");
+      await passport.authenticate("local")(req, res, function () {
+        res.redirect("/secrets");
+      });
     } catch (err) {
       console.log(err.message);
       res.redirect("/register");
@@ -115,6 +116,22 @@ app
     res.render("login");
   })
   .post(async (req, res) => {
+    const user = new User({
+      username: req.body.username,
+      password: req.body.password,
+    });
+
+    req.login(user, (err) => {
+      if (err) {
+        console.log(err);
+        res.redirect("/login");
+      } else {
+        passport.authenticate("local")(req, res, function () {
+          res.redirect("/secrets");
+        });
+      }
+    });
+
     // try {
     //   const user = await User.findOne({ username: req.body.username });
     //   if (!user) throw new Error("User does not exist.");
@@ -136,15 +153,15 @@ app
   });
 
 app.route("/logout").get((req, res) => {
-  res.redirect("/");
+  req.logout((err) => {
+    if (!err) res.redirect("/");
+  });
 });
 
 app.get("/secrets", (req, res) => {
-  if (req.isAuthenticated()) {
-    res.render("secrets");
-  } else {
-    res.redirect("/login");
-  }
+  req.isAuthenticated()
+    ? res.render("secrets", { username: req.session.passport.user })
+    : res.redirect("/login");
 });
 
 app.listen("3000", () => console.log("Server started on port 3000."));
